86b508846c221336106f8c3c9e8f838243b68521
James Bell <jtb2013@gmail.com>
2018-11-20 11:39:08 -0600

    initial commit

---
+++ src/camera_to_inertial.cpp
@@ -0,0 +1,40 @@
+//TODO: tAKE IN NAME OF TOPICS TO CONVERT FROM, TO, AND USE AS CAMERA INFO
+#include <ros/ros.h>
+#include <time.h>
+#include <geometry_msgs/Point.h>
+#include <Eigen/Dense> 
+
+#include "pv_estimator/State.h"
+#include "estimator.h"
+
+#include <string>
+#include <cstring>
+#include <array>
+
+
+
+ros::Publisher pub;
+ros::Subscriber sub;
+
+void pvCallback(pv_estimator::State msg){
+  geometry_msgs::Vector3 vec;
+  vec.x = msg.r[0]*.01/640/.005;
+  vec.y = msg.r[1]*.01/480/.005;
+  vec.z = 1;
+  pub.publish(vec);
+}
+
+int main(int argc, char** argv){
+  ros::init(argc,argv,"camera_to_inertial");//This should change based on which nodes you're converting, how many of this type are already running
+  ros::NodeHandle nh;
+
+  pub = nh.advertise<geometry_msgs::Vector3>(std::string("/tracker/inertial"),1000);
+  sub = nh.subscribe<pv_estimator::State(std::string("/tracker/state"),1000,pvCallback);
+  ros::spin();
+}
+
+
+
+
+
+
